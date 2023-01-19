0b1412e381d9b75afa501e052c69aa996951d66d
Jovan Milovanovic <jovan.milovanovic@finspot.rs>
2023-01-19 18:46:43 +0100

    rename folder; setup backend

--- dffbackend/Controllers/WeatherForecastController.cs
+++ dffbackend/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace backend.Controllers;
+namespace dffbackend.Controllers;
 
 [ApiController]
 [Route("[controller]")]
--- dffbackend/WeatherForecast.cs
+++ dffbackend/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace backend;
+namespace dffbackend;
 
 public class WeatherForecast
 {
