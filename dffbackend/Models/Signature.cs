b7aea011b46f17c41e7bc8bad13c1a7ec525ec06
Jovan Milovanovic <jovan.milovanovic@finspot.rs>
2023-01-20 14:34:20 +0100

    create dbcontext and models

---
+++ dffbackend/Models/Signature.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace dffbackend.Models;
+
+public class Signature
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public Guid FactoringCompanyId { get; set; }
+
+    [Required]
+    public FactoringCompany FactoringCompany { get; set; }
+
+    public string Signature1 { get; set; }
+    
+    public string Signature2 { get; set; }
+
+    public string Signature3 { get; set; }
+
+    public string Signature4 { get; set; }
+}
