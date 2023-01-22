c0fcb743d8f22c0b639574ff399884c0e9978163
Jovan Milovanovic <jovan.milovanovic@finspot.rs>
2023-01-22 16:39:00 +0100

    check signatures for duplicates and store in db

--- dffbackend/Controllers/SignaturesController.cs
+++ dffbackend/Controllers/SignaturesController.cs
@@ -41,8 +41,32 @@ public class SignaturesController : BaseController
         });
 
         return Ok(result);
-    }  
+    }
+
+    /// <summary>
+    /// Checks signature sets for duplicates and stores them in db
+    /// </summary>
+    /// <param name="body">CheckSignaturesDto containing a list of signature sets</param>
+    /// <response code="200">Ok</response>
+    /// <response code="200">OkObject</response>
+    /// <response code="400">Invalid input</response>
+    /// <response code="401">User not authorized</response>
+    [HttpPost]
+    [Route("checkandstore")]
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+    public async Task<IActionResult> CheckSignaturesAndStore([FromBody] CheckSignaturesDto body)
+    {
+        // 2 endpoint - proverava duplikate i storuje taj entry (checkandstore)
+        var result = await Mediator.Send(new CheckSignaturesAndStoreCommand
+        {
+            SignaturesSets = body.SignaturesSets
+        });
+
+        return Ok(result);
+    }
 }
 
-// 2 endpoint - proverava duplikate i storuje taj entry (checkandstore)
+
 // 3 endpoint - CRUD nad faktorima (flag obrisan) (ovo ide u FactoringCompanyController)
