3aa7cf56d77f1de20a91b6597f3aefeb178aab34
Ognjen Kurtić <kurticognjen@gmail.com>
2023-01-24 11:56:03 +0100

    Refactor request\response processing on the frontend

--- dfffrontend/app.js
+++ dfffrontend/app.js
@@ -42,12 +42,46 @@ btnGenSig.addEventListener("click", async function (event) {
 btnChkDups.addEventListener("click", async function (event) {
 	event.preventDefault();
 
+    const response = await fetch('http://localhost:5296/api/Signatures/check', {
+        headers: fetchApiKeyAndPrepareHeaders(),
+    	method: 'POST',
+        body: JSON.stringify(fetchSignaturesAndPrepareBody()),
+    });
+
+    await processResponse(response);
+});
+
+btnSendSig.addEventListener("click", async function (event) {
+	event.preventDefault();
+
+    const response = await fetch('http://localhost:5296/api/Signatures/checkandstore', {
+        headers: fetchApiKeyAndPrepareHeaders(),
+    	method: 'POST',
+        body: JSON.stringify(fetchSignaturesAndPrepareBody()),
+    });
+
+    await processResponse(response);
+});
+
+async function hashCode(str) {
+    console.log(`Applying SHA-256 on the following string: ` + str)
+    const textAsBuffer = new TextEncoder().encode(str);
+    const hashBuffer = await window.crypto.subtle.digest('SHA-256', textAsBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer))
+    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+}
+
+function fetchApiKeyAndPrepareHeaders() {
     const apiKey = authForm.elements["api_key"].value;
 
     const requestHeaders = new Headers();
     requestHeaders.append('Content-Type', 'application/json');
-    requestHeaders.append('X-Factor-API-Key', apiKey.textContent);
+    requestHeaders.append('X-Factor-API-Key', apiKey);
 
+    return requestHeaders;
+}
+
+function fetchSignaturesAndPrepareBody() {
     const signatureSet = {
         signature1: pSignature1.textContent,
         signature2: pSignature2.textContent,
@@ -59,25 +93,29 @@ btnChkDups.addEventListener("click", async function (event) {
         signatureSets: [signatureSet]
     }
 
-    const response = await fetch('http://localhost:5296/api/Signatures/check', {
-        headers: requestHeaders,
-    	method: 'POST',
-        body: JSON.stringify(requestBody),
-    });
+    return requestBody;
+}
 
+async function processResponse(response) {
     if (!response.ok) {
-        result.textContent = "Došlo je do greške!";
-        result.className = "signature-alert";
-        console.warn('Something went wrong.', await response.json());
+        processErrorResponse(response);
         return;
     }
 
     const responseData = await response.json();
     console.log(JSON.stringify(responseData));
 
-    // if (data.isDuplicate) {
-    //     result.textContent = "Faktura je bila predmet faktoringa.";
-    //     result.className = "signature-alert";
+    if (responseData.length === 0)
+    {
+        result.textContent = "Faktura nije bila predmet faktoringa.";
+        result.className = "signature-ok";
+    } else {
+        result.textContent = "Faktura je bila predmet faktoringa.";
+        result.className = "signature-alert";
+    }
+
+        // if (data.isDuplicate) {
+
 
     //     if (data.duplicateSignature1 == pSignature1.textContent) {
     //         pSignature1.className = "signature-alert";
@@ -90,78 +128,22 @@ btnChkDups.addEventListener("click", async function (event) {
     //     if (data.duplicateSignature3 == pSignature3.textContent) {
     //         pSignature3.className = "signature-alert";
     //     }
+}
 
-    // } else {
-    //     result.textContent = "Faktura nije bila predmet faktoringa.";
-    //     result.className = "signature-ok";
-    // }
-});
+async function processErrorResponse(response) {
+    result.className = "signature-alert";
 
-btnSendSig.addEventListener("click", function (event) {
-	event.preventDefault();
-
-    const apiKey = authForm.elements["api_key"].value;
-
-    const requestHeaders = new Headers();
-    requestHeaders.append('Content-Type', 'application/json');
-    requestHeaders.append('X-Factor-API-Key', apiKey.textContent);
-
-    const signatureSet = {
-        signature1: pSignature1.textContent,
-        signature2: pSignature2.textContent,
-        signature3: pSignature3.textContent,
-        signature4: pSignature4.textContent,
+    switch (response.status) {
+        case 401:
+            result.textContent = "Neuspešna autorizacija. Proverite API ključ.";
+            break;
+        case 400:
+            result.textContent = "Nevalidan zahtev. Proverite konzolu za više detalja.";
+            break;
+        default:
+            result.textContent = "Došlo je do greške. Proverite konzolu za više detalja.";
+            break;
     }
 
-    const requestBody = {
-        signatureSets: [signatureSet]
-    }
-
-    fetch('http://localhost:5296/api/Signatures/checkandstore', {
-        headers: requestHeaders,
-    	method: 'POST',
-        body: JSON.stringify(requestBody),
-    }).then(function (response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            return Promise.reject(response);
-        }
-    }).then(function (data) {
-
-        console.log(JSON.stringify(data));
-
-        // if (data.isDuplicate) {
-        //     result.textContent = "Faktura je bila predmet faktoringa.";
-        //     result.className = "signature-alert";
-
-        //     if (data.duplicateSignature1 == pSignature1.textContent) {
-        //         pSignature1.className = "signature-alert";
-        //     }
-
-        //     if (data.duplicateSignature2 == pSignature2.textContent) {
-        //         pSignature2.className = "signature-alert";
-        //     }
-
-        //     if (data.duplicateSignature3 == pSignature3.textContent) {
-        //         pSignature3.className = "signature-alert";
-        //     }
-
-        // } else {
-        //     result.textContent = "Faktura nije bila predmet faktoringa.";
-        //     result.className = "signature-ok";
-        // }
-
-    }).catch(function (err) {
-        result.textContent = "Došlo je do greške!";
-        result.className = "signature-alert";
-    });
-});
-
-async function hashCode(str) {
-    console.log(`Applying SHA-256 on the following string: ` + str)
-    const textAsBuffer = new TextEncoder().encode(str);
-    const hashBuffer = await window.crypto.subtle.digest('SHA-256', textAsBuffer);
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
-    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    console.warn('Došlo je do greške. Full response: ', await response.json());
 }
