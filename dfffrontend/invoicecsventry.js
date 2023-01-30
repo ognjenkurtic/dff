3b5da9a1368a976deb4f9494318a958a5503d32b
Ognjen Kurtić <kurticognjen@gmail.com>
2023-01-30 21:53:14 +0100

    Add signature visualization for csv sigs

--- dfffrontend/invoicecsventry.js
+++ dfffrontend/invoicecsventry.js
@@ -1,3 +1,5 @@
+const result_row_csv = document.getElementsByClassName("csv_row_index");
+const result_csv = document.getElementsByClassName("csv_row_result");
 const btnParseCsv = document.getElementById("parse_csv");
 
 btnParseCsv.addEventListener("click", async function (event) {
@@ -30,10 +32,11 @@ btnParseCsv.addEventListener("click", async function (event) {
                 const generatedSignatureSet = await generateSignatures(invoiceRow[0], invoiceRow[1], invoiceRow[2], invoiceRow[3], invoiceRow[4], invoiceRow[5], invoiceRow[6]);
                 console.log(`Generisani potpisi za red ${i + 1}: ${JSON.stringify(generatedSignatureSet)}`);
                 generatedSignatureSets.push(generatedSignatureSet);
+
                 i++;
             }
 
-            // showSignaturesForCsvEntry(generatedSignatureSets);
+            showSignaturesForCsvEntry(generatedSignatureSets);
             
             const response = await fetch(`${apiUrl}/api/Signatures/check`, {
                 headers: fetchApiKeyAndPrepareHeaders(),
@@ -47,3 +50,19 @@ btnParseCsv.addEventListener("click", async function (event) {
 
     reader.readAsText(selectedFile);
 });
+
+function showSignaturesForCsvEntry(singatureSets) {
+    let i = 0;
+    singatureSets.forEach(ss => {
+        i++;
+
+        const new_result_row_index = result_row_csv[i-1].cloneNode();
+        const new_result_row_content = result_csv[i-1].cloneNode();
+
+        new_result_row_index.textContent = `Red ${i}`;
+        new_result_row_content.textContent = `s1:${ss.signature1} s2:${ss.signature2} s3:${ss.signature3} s4:${ss.signature4} `
+
+        result_csv[i-1].after(new_result_row_index);
+        new_result_row_index.after(new_result_row_content);
+    });
+}
