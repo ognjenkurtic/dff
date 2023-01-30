58a93b33a7ea4e7f3495c64cf40332627b35c52a
Ognjen Kurtić <kurticognjen@gmail.com>
2023-01-30 22:19:00 +0100

    Add input validation and sig 5 prep on frontend WIP

--- dfffrontend/invoicemanualentry.js
+++ dfffrontend/invoicemanualentry.js
@@ -8,7 +8,11 @@ const pSignature1 = document.getElementById("potpis_1");
 const pSignature2 = document.getElementById("potpis_2");
 const pSignature3 = document.getElementById("potpis_3");
 const pSignature4 = document.getElementById("potpis_4");
+const pSignature5 = document.getElementById("potpis_5");
 const result = document.getElementById("result");
+
+// Regular expression to check if string is a valid UUID
+const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
 
 btnChkDups.addEventListener("click", async function (event) {
 	event.preventDefault();
@@ -24,6 +28,10 @@ btnChkDups.addEventListener("click", async function (event) {
         iznos,
         sefId 
     ] = getInputsFromEntryForm();
+
+    if (!matBrojDob && !sefId) {
+        return;
+    }
 
     const generatedSignatureSet = await generateSignatures(matBrojDob, matBrojKupac, brojFakture, datumIzdavanja, datumValute, iznos, sefId);
 
@@ -52,6 +60,10 @@ btnSendSig.addEventListener("click", async function (event) {
         iznos,
         sefId 
     ] = getInputsFromEntryForm();
+
+    if (!matBrojDob && !sefId) {
+        return;
+    }
 
     const generatedSignatureSet = await generateSignatures(matBrojDob, matBrojKupac, brojFakture, datumIzdavanja, datumValute, iznos, sefId);
 
@@ -82,6 +94,8 @@ function cleanupSignaturesAndResult() {
     pSignature3.className = '';
     pSignature4.textContent = '';
     pSignature4.className = '';
+    pSignature5.textContent = '';
+    pSignature5.className = '';
 }
 
 function getInputsFromEntryForm() {
@@ -92,6 +106,20 @@ function getInputsFromEntryForm() {
     const datumValute = invoiceForm.elements["datum_valute"].value;
     const iznos = invoiceForm.elements["iznos"].value;
     const sefId = invoiceForm.elements["sef_id"].value; // 766e3a6b-1be5-48ac-bad4-eb12ceba540c
+
+    const sefIdEntered = sefId !== undefined && sefId !== '';
+
+    if (sefIdEntered) {
+        if (!regexExp.test(sefId)) {
+            alert('SEF ID mora biti validan GUID');
+            return [ null ];
+        }
+    } else {
+        if (matBrojDob.length != 8 || matBrojKupac.length != 8) {
+            alert('Matični broj mora imati 8 brojeva');
+            return [ null ];
+        };
+    }
 
     return [matBrojDob, matBrojKupac, brojFakture, datumIzdavanja, datumValute, iznos, sefId];
 }
@@ -115,6 +143,11 @@ function showSignaturesForManualEntry(signatures) {
     if (signatures.signature4 !== undefined) {
         pSignature4.textContent = signatures.signature4;
         pSignature4.className = '';
+    }
+
+    if (signatures.signature5 !== undefined) {
+        pSignature5.textContent = signatures.signature4;
+        pSignature5.className = '';
     }
 }
 
@@ -172,6 +205,7 @@ async function processOkResponse(response, isStoreAction) {
         const sig2Duplicate = signatureSetCheckResult.signatureDuplicateResponses.find(sdr => sdr.signatureType === 2);
         const sig3Duplicate = signatureSetCheckResult.signatureDuplicateResponses.find(sdr => sdr.signatureType === 3);
         const sig4Duplicate = signatureSetCheckResult.signatureDuplicateResponses.find(sdr => sdr.signatureType === 4);
+        const sig5Duplicate = signatureSetCheckResult.signatureDuplicateResponses.find(sdr => sdr.signatureType === 5);
 
         if (sig1Duplicate) {
             factorName = sig1Duplicate.factoringCompanyName;
@@ -195,6 +229,12 @@ async function processOkResponse(response, isStoreAction) {
             factorName = sig4Duplicate.factoringCompanyName;
             factorEmail = sig4Duplicate.email;
             pSignature4.className = "signature-alert";
+        }
+
+        if (sig5Duplicate) {
+            factorName = sig5Duplicate.factoringCompanyName;
+            factorEmail = sig5Duplicate.email;
+            pSignature5.className = "signature-alert";
         }
 
         result.textContent = `Faktura je bila predmet faktoringa kod ${factorName}. Kontakt email: ${factorEmail}`;
