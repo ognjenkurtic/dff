541e27a746f94751ab0e56d9168f35e92ac3bc0e
dramicaninstefan <dramicanin.stefan@gmail.com>
2024-09-19 16:17:10 +0200

    Relocated the code to the right places

--- dfffrontend/auth.js
+++ dfffrontend/auth.js
@@ -5,6 +5,9 @@ const authForm = document.getElementById("auth_data");
 function fetchApiKeyAndPrepareHeaders() {
   const apiKey = authForm.elements["api_key"].value;
 
+  // Storing api key to local storage
+  localStorage.setItem("api_key", apiKey);
+
   const requestHeaders = new Headers();
   requestHeaders.append("Content-Type", "application/json");
   requestHeaders.append("X-Factor-API-Key", apiKey);
